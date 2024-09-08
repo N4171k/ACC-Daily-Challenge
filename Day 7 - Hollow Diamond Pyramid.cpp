6299f9db2d7490fa41fc34f9ae7d69b2729df3f7
Naitik Tiwari <144876588+N4171k@users.noreply.github.com>
2024-09-08 22:41:08 +0530

    Create Day 7 - Hollow Diamond Pyramid.cpp

---
+++ Day 7 - Hollow Diamond Pyramid.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter N: ";
+    cin >> n;
+
+    for (int i = 0; i < n / 2; i++) {
+        for (int j = 0; j < n / 2 - i; j++) {
+            cout << "*";
+        }
+        for (int j = 0; j < 2 * i + 1; j++) {
+            cout << " ";
+        }
+        for (int j = 0; j < n / 2 - i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i <= n / 2; i++) {
+        for (int j = 0; j <= i; j++) {
+            cout << "*";
+        }
+        for (int j = 0; j < n - 2 * (i + 1); j++) {
+            cout << " ";
+        }
+        for (int j = 0; j <= i; j++) {
+            if (j != n / 2 || i != n / 2) {
+                cout << "*";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
